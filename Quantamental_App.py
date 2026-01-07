60ce388cd1268abbc13232569495e828e8060ded
wogus337 <wogus337@naver.com>
2026-01-07 15:35:18 +0900

    Update Quantamental_App.py

--- Quantamental_App.py
+++ Quantamental_App.py
@@ -1778,7 +1778,7 @@ with tab1:
             transposed,
             gridOptions=gb.build(),
             height=400,
-            width='60%',
+            width='100%',
             fit_columns_on_grid_load=True,
             theme="streamlit",
             allow_unsafe_jscode=True
@@ -1875,7 +1875,7 @@ with tab1:
             transposed,
             gridOptions=gb.build(),
             height=400,
-            width='60%',
+            width='100%',
             fit_columns_on_grid_load=True,
             theme="streamlit",
             allow_unsafe_jscode=True
@@ -2312,6 +2312,7 @@ with tab2:
 
     with subtab2:
         st.subheader("Transformer FX Signal")
+
 
 
 
